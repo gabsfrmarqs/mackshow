d0c9e5e7ad016149f1aa80f3b44af0613998e842
Cloud User <ec2-user@ip-172-31-86-65.ec2.internal>
2023-08-25 10:59:46 +0000

    lab02 adicionado

---
+++ lab02/exercicio2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main{
+	int numNotas = 3;
+	int notas[3] = {8,4,5,8};
+	int soma = notas[0] + notas[1]*2 + notas[2]*3 + notas[3];
+	cout >> "Média: >> soma/7;
+	return 0;
+}
